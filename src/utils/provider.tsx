9f0591b3ececc81b05027fc201a1c99fdf1d3182
Woojin Lee <nijoow1127@gmail.com>
2023-05-03 22:58:13 +0900

    🎨 recoil root 추가

--- src/utils/provider.tsx
+++ src/utils/provider.tsx
@@ -2,10 +2,15 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
 
 export const Providers = ({ children }: React.PropsWithChildren) => {
   const [client] = useState(
     new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
   );
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>{" "}
+    </RecoilRoot>
+  );
 };
