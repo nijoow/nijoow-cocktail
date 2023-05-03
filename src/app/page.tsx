3c9eddb61a74110a49b35d8d9d004c459861bcee
Woojin Lee <nijoow1127@gmail.com>
2023-05-03 22:59:55 +0900

    ✨ list viewer 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,14 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import axios from "axios";
-import Filter from "./Filter";
-const inter = Inter({ subsets: ["latin"] });
-
-async function getData() {
-  const res = await fetch(
-    "https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list"
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
+import Filter from "@/components/Filter";
+import Viewer from "@/components/Viewer";
 
 export default async function Home() {
-  const data = await getData();
-  console.log(data);
   return (
-    <main className="flex items-center justify-between h-full min-h-screen p-24">
-      <div className="w-full h-full max-w-xs bg-red-300">1</div>
-      <div className="flex-auto h-full bg-blue-300">
+    <main className="flex items-center justify-between h-full min-h-screen ">
+      <div className="w-full h-full max-w-md">
         <Filter />
+      </div>
+      <div className="flex-auto h-full overflow-auto">
+        <Viewer />
       </div>
     </main>
   );
---
+++ src/components/Viewer.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { filterAtom, selectedValueAtom } from "@/recoil/atom";
+import { useGetFilteredItemListQuery } from "@/services/api";
+import { useRecoilState } from "recoil";
+import React from "react";
+import Image from "next/image";
+
+const Viewer = () => {
+  const [filter] = useRecoilState(filterAtom);
+  const [selectedValue] = useRecoilState(selectedValueAtom);
+
+  const { data } = useGetFilteredItemListQuery(filter, selectedValue);
+
+  return (
+    <div className="grid grid-cols-12 gap-12 p-24">
+      {(data?.drinks || []).map(
+        (drink: {
+          idDrink: string;
+          strDrink: string;
+          strDrinkThumb: string;
+        }) => (
+          <div
+            key={drink.idDrink}
+            className="col-span-3 card w-full bg-base-100 shadow-xl"
+          >
+            <figure className="relative w-full h-0 pb-[100%]">
+              <Image
+                src={drink.strDrinkThumb}
+                alt={drink.strDrink}
+                fill
+                className="object-cover"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{drink.strDrink}</h2>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default Viewer;
