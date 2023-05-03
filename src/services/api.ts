a5c624efe691a4cc654612ce533234778ecc03e0
Woojin Lee <nijoow1127@gmail.com>
2023-05-03 22:57:59 +0900

    🎨 필터링된 칵테일 불러오는 api

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,3 +15,20 @@ export const useGetFilterListQuery = (type: FilterType) => {
   );
   return { data, isLoading };
 };
+
+const getFilteredItemListApi = async (type: FilterType, value: string) => {
+  const { data } = await axios.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?${FilterEnum[type]}=${value}`
+  );
+  return data;
+};
+
+export const useGetFilteredItemListQuery = (
+  type: FilterType,
+  value: string
+) => {
+  const { data, isLoading } = useQuery(["FilteredItemList", type, value], () =>
+    getFilteredItemListApi(type, value)
+  );
+  return { data, isLoading };
+};
