47c631956e27c07e19b0ed8051cce18b96f0e207
Woojin Lee <nijoow1127@gmail.com>
2023-05-09 00:12:37 +0900

    🚧 사이드바 임시 제거

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,8 +1,11 @@
 import { FilterType } from "@/types/types";
 import { atom } from "recoil";
 
+export const openSidebarAtom = atom({ key: "OpenSidebar", default: true });
+
 export const filterAtom = atom<FilterType>({
   key: "Filter",
   default: "Categories",
 });
+
 export const selectedValueAtom = atom({ key: "SelectedValue", default: "" });
