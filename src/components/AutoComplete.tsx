7bb963d8dc20c0ae8f35417368e5a9e5959fb9a2
Woojin Lee <nijoow1127@gmail.com>
2023-05-10 23:57:02 +0900

    🎨 AutoComplete 컴포넌트 수정

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,18 +1,37 @@
 import React, { memo, useCallback, useEffect, useRef, useState } from "react";
+import { SetterOrUpdater } from "recoil";
+import CloseIcon from "./CloseIcon";
 
 type AutocompleteProps = {
   options: string[];
-  value: string;
-  setValue(val: string): void;
+  value: string | string[];
+  setValue: any;
 };
 
 const AutoComplete = ({ options = [], value, setValue }: AutocompleteProps) => {
   const ref = useRef<HTMLDivElement>(null);
+  const [text, setText] = useState<string>("");
+  const [open, setOpen] = useState(false);
   const [filteredOptions, setFilteredOptions] = useState<string[]>(options);
 
   useEffect(() => {
-    setFilteredOptions(options.filter((option) => option.includes(value)));
-  }, [value, options]);
+    setFilteredOptions(options.filter((option) => option.includes(text)));
+  }, [text, options]);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => document.addEventListener("click", handleClickOutside);
+  }, []);
+
+  const handleClickOutside = (e: any) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+
+  const isSelectedOption = (option: string) => {
+    return value.includes(option);
+  };
 
   const handleInputChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,62 +40,69 @@ const AutoComplete = ({ options = [], value, setValue }: AutocompleteProps) => {
     []
   );
 
+  const handleClickOption = (option: string) => {
+    if (isSelectedOption(option)) return;
+
+    setText(option);
+    if (typeof value === "object") {
+      setValue([...value, option]);
+    } else {
+      setValue(option);
+    }
+    setOpen(false);
+  };
+
   return (
-    <div className="w-full dropdown" ref={ref}>
-      <div className="relative">
+    <div className="w-full" ref={ref}>
+      <div className="relative flex">
         <input
           type="text"
-          className="w-full input-sm input-bordered rounded-md"
-          value={value}
+          className="w-full py-2 px-4  input-bordered rounded-md"
+          value={text}
           onChange={handleInputChange}
           placeholder="search"
           tabIndex={0}
+          onFocus={() => setOpen(true)}
         />
         <button
-          className="btn btn-square btn-sm btn-outline absolute right-0"
+          className=" h-10 w-10 flex items-center justify-center rounded-md border transition-all hover:bg-gray-400 border-white absolute right-0"
           type="button"
           onClick={(e) => {
             e.stopPropagation();
-            setValue("");
+            setText("");
+            setOpen(true);
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <CloseIcon className="h-6 w-6 stroke-whte hover:stroke-gray-900 transition-all" />
         </button>
       </div>
-      <div className="flex-col overflow-y-auto rounded-md dropdown-content bg-base-200 max-h-96">
-        <ul
-          className="menu menu-compact "
-          style={{ width: ref.current?.clientWidth as number }}
-        >
-          {filteredOptions.map((option, index) => {
-            return (
-              <li
-                key={index}
-                tabIndex={index + 1}
-                onClick={() => {
-                  setValue(option);
-                }}
-                className="w-full border-b border-b-base-content/10"
-              >
-                <button>{option}</button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {open && (
+        <div className="z-30 absolute flex-col overflow-x-hidden overflow-y-auto rounded-md dropdown-content bg-base-200 max-h-96">
+          <ul
+            className="menu menu-compact "
+            style={{
+              width:
+                (ref.current?.clientWidth as number) -
+                (filteredOptions.length > 1 ? 14 : 0),
+            }}
+          >
+            {filteredOptions.map((option, index) => {
+              return (
+                <li
+                  key={index}
+                  tabIndex={index + 1}
+                  onClick={() => handleClickOption(option)}
+                  className={`w-full border-b border-b-base-content/10  ${
+                    isSelectedOption(option) ? "bg-gray-600" : ""
+                  }`}
+                >
+                  <button>{option}</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
